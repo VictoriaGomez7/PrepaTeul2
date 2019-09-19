c19fa96de316da296f7bfdaa7345adaf3a18f130
aldairleyva <50346143+aldairleyva@users.noreply.github.com>
2019-09-18 23:17:57 -0500

    Revert "CIERRE DE CICLO"
    This reverts commit 4b63c36c35f3ee9dfcbf645bb45c0e299ca57ece.

--- routes/web.php
+++ routes/web.php
@@ -143,5 +143,3 @@ Route::resource('AsignarCalificacion','CalificacionesController');
 Route::resource('Calificaciones','CalificacionesController');
 
 Route::resource('Irregulares', 'IrregularController');
-
-Route::resource('Cerrar_Semestre', 'CierreController');
