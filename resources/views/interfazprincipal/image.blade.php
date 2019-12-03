4b69b2f159fec57a3ddfc5b89e5ed263f7cc3a4c
Victoria gomez <>
2019-12-03 11:39:15 -0600

    Cambio de la magen del logo

--- resources/views/interfazprincipal/image.blade.php
+++ resources/views/interfazprincipal/image.blade.php
@@ -15,7 +15,7 @@
 		<div class="ContenImages" id="unouno"><img class="card-img-top" src="images/{{$ImagenesEnDB[0]->Imagen}}" style="height: 180px; width: 500px" ></div>
 		<div class="ContenImages" id="dosdos"><img class="card-img-top" src="images/{{$ImagenesEnDB[1]->Imagen}}" style="height: 180px" ></div>
 		<div class="ContenImages" id="trestres"><img class="card-img-top" src="images/{{$ImagenesEnDB[2]->Imagen}}"  style="height: 180px" ></div>
-		<div class="ContenImages" id="cuatrocuatro"><img class="card-img-top" src="images/{{$ImagenesEnDB[3]->Imagen}}" style="height: 180px; width: 180px"></div>
+		<div class="ContenImages" id="cuatrocuatro"><img class="card-img-top" src="images/{{$ImagenesEnDB[3]->Imagen}}" style="height: 180px; width: 240px"></div>
 		
 		
 		
