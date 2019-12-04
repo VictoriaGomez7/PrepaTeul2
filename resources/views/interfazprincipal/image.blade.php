034e514863c573df36b5c512c24cbad8161e8280
JoRg1t0 <48129665+JoRg1t0@users.noreply.github.com>
2019-12-04 10:02:58 -0600

    logos error

--- resources/views/interfazprincipal/image.blade.php
+++ resources/views/interfazprincipal/image.blade.php
@@ -10,12 +10,38 @@
 <body>
 		 
 		<?php use App\Logos;
-					$ImagenesEnDB=Logos::all();
+		$totalogos=Logos::where('Nombre_Imagen','Imagen1')->get();
+        $sinimagen='SinImagen.jpg';
+        //return $sinimagen;
+        if (count($totalogos)==0) {
+            $nuevoLogo= new Logos();
+            $nuevoLogo->Nombre_Imagen='Imagen1';
+            $nuevoLogo->Imagen=$sinimagen;
+            $nuevoLogo->save();}
+        $totalogos=Logos::where('Nombre_Imagen','Imagen2')->get();
+        if (count($totalogos)==0) {
+            $nuevoLogo= new Logos();
+            $nuevoLogo->Nombre_Imagen='Imagen2';
+            $nuevoLogo->Imagen=$sinimagen;
+            $nuevoLogo->save();}
+        $totalogos=Logos::where('Nombre_Imagen','Imagen3')->get();
+        if (count($totalogos)==0) {
+            $nuevoLogo= new Logos();
+            $nuevoLogo->Nombre_Imagen='Imagen3';
+            $nuevoLogo->Imagen=$sinimagen;
+            $nuevoLogo->save();}
+        $totalogos=Logos::where('Nombre_Imagen','Imagen4')->get();
+        if (count($totalogos)==0) {
+            $nuevoLogo= new Logos();
+            $nuevoLogo->Nombre_Imagen='Imagen4';
+            $nuevoLogo->Imagen=$sinimagen;
+            $nuevoLogo->save();}
+		$ImagenesEnDB=Logos::all();
 				?>
-		<div class="ContenImages" id="unouno"><img class="card-img-top" src="images/{{$ImagenesEnDB[0]->Imagen}}" style="height: 150px;" ></div>
-		<div class="ContenImages" id="dosdos"><img class="card-img-top" src="images/{{$ImagenesEnDB[1]->Imagen}}" style="height: 150px;" ></div>
-		<div class="ContenImages" id="trestres"><img class="card-img-top" src="images/{{$ImagenesEnDB[2]->Imagen}}"  style="height: 150px;" ></div>
-		<div class="ContenImages" id="cuatrocuatro"><img class="card-img-top" src="images/{{$ImagenesEnDB[3]->Imagen}}" style="height: 150px;"></div>
+		<div class="ContenImages" id="unouno"><img class="card-img-top" src="/images/{{$ImagenesEnDB[0]->Imagen}}" style="height: 150px;" ></div>
+		<div class="ContenImages" id="dosdos"><img class="card-img-top" src="/images/{{$ImagenesEnDB[1]->Imagen}}" style="height: 150px;" ></div>
+		<div class="ContenImages" id="trestres"><img class="card-img-top" src="/images/{{$ImagenesEnDB[2]->Imagen}}"  style="height: 150px;" ></div>
+		<div class="ContenImages" id="cuatrocuatro"><img class="card-img-top" src="/images/{{$ImagenesEnDB[3]->Imagen}}" style="height: 150px;"></div>
 		
 		
 		
--- storage/framework/views/61e10c8a96a4cb4d9c94aff104d2cb038b8edca8.php
+++ storage/framework/views/61e10c8a96a4cb4d9c94aff104d2cb038b8edca8.php
@@ -3,8 +3,48 @@
 <head>
 	<title></title>
 </head>
+<header>	
+	<link rel="stylesheet" type="text/css" href="/css/imagenes.css">
+		 
+</header>
 <body>
-		 <div align="center">
-        <img src="/images/img2c.jpeg"  style=" width:100% ; height: 21% "></div>
+		 
+		<?php use App\Logos;
+		$totalogos=Logos::where('Nombre_Imagen','Imagen1')->get();
+        $sinimagen='SinImagen.jpg';
+        //return $sinimagen;
+        if (count($totalogos)==0) {
+            $nuevoLogo= new Logos();
+            $nuevoLogo->Nombre_Imagen='Imagen1';
+            $nuevoLogo->Imagen=$sinimagen;
+            $nuevoLogo->save();}
+        $totalogos=Logos::where('Nombre_Imagen','Imagen2')->get();
+        if (count($totalogos)==0) {
+            $nuevoLogo= new Logos();
+            $nuevoLogo->Nombre_Imagen='Imagen2';
+            $nuevoLogo->Imagen=$sinimagen;
+            $nuevoLogo->save();}
+        $totalogos=Logos::where('Nombre_Imagen','Imagen3')->get();
+        if (count($totalogos)==0) {
+            $nuevoLogo= new Logos();
+            $nuevoLogo->Nombre_Imagen='Imagen3';
+            $nuevoLogo->Imagen=$sinimagen;
+            $nuevoLogo->save();}
+        $totalogos=Logos::where('Nombre_Imagen','Imagen4')->get();
+        if (count($totalogos)==0) {
+            $nuevoLogo= new Logos();
+            $nuevoLogo->Nombre_Imagen='Imagen4';
+            $nuevoLogo->Imagen=$sinimagen;
+            $nuevoLogo->save();}
+		$ImagenesEnDB=Logos::all();
+				?>
+		<div class="ContenImages" id="unouno"><img class="card-img-top" src="/images/<?php echo e($ImagenesEnDB[0]->Imagen); ?>" style="height: 150px;" ></div>
+		<div class="ContenImages" id="dosdos"><img class="card-img-top" src="/images/<?php echo e($ImagenesEnDB[1]->Imagen); ?>" style="height: 150px;" ></div>
+		<div class="ContenImages" id="trestres"><img class="card-img-top" src="/images/<?php echo e($ImagenesEnDB[2]->Imagen); ?>"  style="height: 150px;" ></div>
+		<div class="ContenImages" id="cuatrocuatro"><img class="card-img-top" src="/images/<?php echo e($ImagenesEnDB[3]->Imagen); ?>" style="height: 150px;"></div>
+		
+		
+		
+        <!--<img src="/images/img2c.jpeg"  style=" width:100% ; height: 21% ">--></div>
 </body>
 </html><?php /**PATH C:\xampp\htdocs\PrepaTeul2\resources\views/interfazprincipal/image.blade.php ENDPATH**/ ?>
