8ee6ff84f51c66902ac833c74baa008b3d4c5d57
DanielSotelo <49506874+DanielSotelo@users.noreply.github.com>
2019-12-05 17:41:20 -0600

    Cifrar contraseñas

--- database/seeds/UsuarisMaestrossSeeder.php
+++ database/seeds/UsuarisMaestrossSeeder.php
@@ -13,55 +13,55 @@ class UsuarisMaestrossSeeder extends Seeder
     {
         DB::table('usuariomaestros')->insert([
             'Usuario' => '1234',
-            'Password'=>'12345'
+          'Password'=>Crypt::encrypt('12345')
         ]);
 
         DB::table('usuariomaestros')->insert([
             'Usuario' => '1235',
-            'Password'=>'12345'
+          'Password'=>Crypt::encrypt('12345')
         ]);
 
         DB::table('usuariomaestros')->insert([
             'Usuario' => '1236',
-            'Password'=>'12345'
+          'Password'=>Crypt::encrypt('12345')
         ]);
 
         DB::table('usuariomaestros')->insert([
             'Usuario' => '1237',
-            'Password'=>'12345'
+          'Password'=>Crypt::encrypt('12345')
         ]);
 
 
         DB::table('usuariomaestros')->insert([
             'Usuario' => '1238',
-            'Password'=>'12345'
+          'Password'=>Crypt::encrypt('12345')
         ]);
 
 
         DB::table('usuariomaestros')->insert([
             'Usuario' => '1239',
-            'Password'=>'12345'
+          'Password'=>Crypt::encrypt('12345')
         ]);
 
 
         DB::table('usuariomaestros')->insert([
             'Usuario' => '12341',
-            'Password'=>'12345'
+          'Password'=>Crypt::encrypt('12345')
         ]);
 
         DB::table('usuariomaestros')->insert([
             'Usuario' => '12342',
-            'Password'=>'12345'
+          'Password'=>Crypt::encrypt('12345')
         ]);
 
         DB::table('usuariomaestros')->insert([
             'Usuario' => '12343',
-            'Password'=>'12345'
+          'Password'=>Crypt::encrypt('12345')
         ]);
 
         DB::table('usuariomaestros')->insert([
             'Usuario' => '12344',
-            'Password'=>'12345'
+          'Password'=>Crypt::encrypt('12345')
         ]);
     }
 }
